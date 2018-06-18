dfc2753742cbab8ecfc355a77721e3dbe46d2435
tiger-tooth <2936362563@qq.com>
2018-06-18 22:43:44 +0800

    可变参数

--- Python-Learn/LiaoXueFeng-Python/define_function.py
+++ Python-Learn/LiaoXueFeng-Python/define_function.py
@@ -7,20 +7,22 @@ import math
 
 
 def my_abs(x):
-    if not isinstance(x,(int,float)):
+    if not isinstance(x, (int, float)):
         raise TypeError('bad opreand type')
-    if x>=0:
+    if x >= 0:
         return x
     else:
         return -x
 
-def move(x,y,step,angle=0):
-    nx=x+step*math.cos(angle)
-    ny=y-step*math.sin(angle)
-    return nx,ny
 
-x,y=move(100,100,60,math.pi/6)
-print(x,y)
+def move(x, y, step, angle=0):
+    nx = x+step*math.cos(angle)
+    ny = y-step*math.sin(angle)
+    return nx, ny
+
+
+x, y = move(100, 100, 60, math.pi/6)
+print(x, y)
 
 # print (my_abs('n'))
 print("hello world!")
---
+++ Python-Learn/LiaoXueFeng-Python/函数的参数.py
@@ -0,0 +1,42 @@
+# def power(x, n=2):
+#     s = 1
+#     while n > 0:
+#         n = n-1
+#         s = s*x
+#     return s
+###########
+
+# print(power(5))
+# print(power(5,3))
+
+# def add_end(L=None):
+#     if L is None:
+#         L=[]
+#     L.append('END')
+#     return L
+
+# print(add_end())
+# print(add_end())
+
+#######################
+
+
+# def calc(numbers):
+#     sum = 0
+#     for n in numbers:
+#         sum = sum+n*n
+#     return sum
+
+
+# print(calc([1, 2, 3]))
+############################
+
+# def calc(*num):
+#     sum=0
+#     for n in num:
+#         sum=sum+n*n
+#     return sum
+# print(calc(1,2,3))
+# nums=[1,2,3]
+# print(calc(*nums))
+################################
